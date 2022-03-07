504b172ad0837ee9c208d98104fd2b4320d99f7a
Paul David Clark <pd@pd.cm>
2022-03-07 07:47:45 -0800

    wp-cli command: wp quiz generate

--- gravityforms-quiz-enhanced.php
+++ gravityforms-quiz-enhanced.php
@@ -5,61 +5,152 @@
  * @package     GFQuiz
  *
  * @wordpress-plugin
- * Plugin Name: GF Quiz Add-On
+ * Plugin Name: GF Quiz Shuffler
  * Plugin URI: http://www.gravityforms.com
- * Description: Integrates Gravity Forms with Quiz
- * Version: 1.0.0
+ * Description: Generate new randomized forms from Question Bank form with command <code>wp quiz generate</code>
+ * Version: 1.0.1
  * Requires at least: 4.0
  * Requires PHP: 5.6
  * Author: rtCamp, thelovekesh
  * Author URI: http://www.rtcamp.com
- * Text Domain: gf-quiz
+ * Text Domain: gfquiz
  * Domain Path: /languages
  * Update URI: http://www.rtcamp.com
  *
  * Copyright 2009-2021 rtCamp Inc.
  */
 
+namespace GF_Quiz_Shuffler;
+
 // Do not access plugin file directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// Define plugin constants.
-define( 'GF_RT_QUIZ_VERSION', '1.0.0' );
-define( 'GF_RT_QUIZ_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+if ( ! function_exists( 'get_plugin_data' ) ) {
+	require_once ABSPATH . 'wp-admin/includes/plugin.php';
+}
+define( 'GF_RT_QUIZ_PLUGIN_VERSION', get_plugin_data( __FILE__ )['Version'] );
+define( 'GF_RT_QUIZ_PLUGIN_PATH', __DIR__ );
 define( 'GF_RT_QUIZ_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
-// Define all actions and filters needed in main plugin file.
-add_action( 'gform_loaded', [ 'GF_RT_Quiz_Bootstrap', 'load' ], 5 );
+add_action(
+	'cli_init',
+	function(){
+		\WP_CLI::add_command( 'quiz generate', function( $args, $assoc_args ){
+			$forms = \GFAPI::get_forms();
 
-/**
- * Class GF_RT_Quiz_Bootstrap
- * Handles the loading of the GF Quiz Add-On
- * Registers GF Quiz Add-On with the GF Add-On framework.
- *
- * @since 1.0.0
- * @package GF_Quiz
- */
-class GF_RT_Quiz_Bootstrap {
+			foreach( $forms as $form ) {
+				if ( 'Question Bank' === $form['title'] ) {
+					$qb_form = $form;
+				}
+			}
 
-	/**
-	 * If the GF Feed Add-On is installed, load load GF Quiz Add-On.
-	 *
-	 * @access public
-	 * @static
-	 */
-	public static function load() {
-		// Check and include GF Feed Add-On Framework.
-		if ( ! method_exists( 'GFForms', 'include_feed_addon_framework' ) ) {
-			return;
-		}
-		GFForms::include_feed_addon_framework();
+			$qb_form['title'] = 'Quiz Variation ' . rand();
+			$qb_form['description'] = 'Quiz Variation';
 
-		// Load GF Quiz Add-On.
-		require_once GF_RT_QUIZ_PLUGIN_PATH . '/class-gravityforms-quiz-enhanced.php';
+			$new_form = generate_form_from_question_bank_form( $qb_form );
 
-		// Register GF Quiz Add-On.
-		GFAddOn::register( 'Gravityforms_Quiz_Enhanced' );
+			$new_form_id = \GFAPI::add_form( $new_form );
+
+			\WP_CLI::log(
+				sprintf(
+					__( 'New form created: %s', 'gfquiz' ),
+					admin_url( 'admin.php?page=gf_edit_forms&id=' . (int) $new_form_id )
+				)
+			);
+		} );
 	}
+);
+
+function generate_form_from_question_bank_form( $qb_form ) {
+	$form = $qb_form;
+
+	$question_bank_fields = \GFAPI::get_fields_by_type( $qb_form, array( 'quiz' ) );
+
+	if ( empty( $question_bank_fields ) ) {
+		return $form;
+	}
+	
+	// store forms fields based on questionCategory
+	$css_question_category = array();
+	$html_question_category = array();
+	$js_question_category = array();
+	$php_question_category = array();
+
+	// loop through all quiz fields and store them in the correct array
+	foreach ( $question_bank_fields as $field ) {
+		$question_category = $field['questionCategory'];
+		switch ( $question_category ) {
+			case 'css':
+				$css_question_category[] = $field;
+				break;
+			case 'html':
+				$html_question_category[] = $field;
+				break;
+			case 'js':
+				$js_question_category[] = $field;
+				break;
+			case 'php':
+				$php_question_category[] = $field;
+				break;
+		}
+	}
+
+	// shuffle the arrays
+	shuffle( $css_question_category );
+	shuffle( $html_question_category );
+	shuffle( $js_question_category );
+	shuffle( $php_question_category );
+
+	// keep only first 10 fields
+	$css_question_category = array_slice( $css_question_category, 0, 1 );
+	$html_question_category = array_slice( $html_question_category, 0, 1 );
+	$js_question_category = array_slice( $js_question_category, 0, 1 );
+	$php_question_category = array_slice( $php_question_category, 0, 1 );
+
+	// replace form fields with the new ones
+	$form_fields = array_merge( $css_question_category, $html_question_category, $js_question_category, $php_question_category );
+
+	$form['fields'] = array();
+	$page_number = 1;
+	$page_id = 1111; // take a large number for unique id
+
+	$page_break = array( // create a custom page break field
+		'type' => 'page',
+		'id' => $page_id,
+		'displayOnly' => true,
+		'formId' => $form['id'],
+		'pageNumber' => 2,
+		'nextButton' => array(
+			'type' => 'text',
+			'text' => esc_html__( 'Next', 'gfquiz' ),
+		),
+		'previousButton' => array(
+			'type' => 'text',
+			'text' => esc_html__( 'Previous', 'gfquiz' ),
+		)
+	);
+
+	foreach ( $form_fields as $field ) {
+		$field['pageNumber'] = $page_number;
+		$form['fields'][] = $field;
+		
+		if ( $page_number > 1 ) {
+			$page_break['id'] = $page_id + 1;
+			$page_break['pageNumber'] = $page_number + 1;
+		}
+
+		$form['fields'][] = \GF_Fields::create( $page_break );
+
+		$page_id++;
+		$page_number++;
+	}
+
+	// remove last page break
+	array_pop( $form['fields'] );
+
+	unset( $form['id'] );
+
+	return $form;
 }
